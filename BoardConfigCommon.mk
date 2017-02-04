a2d5e880484f73485d1361d4f4609cad793b1b3a
Jesse Chan <jc@linux.com>
2017-02-04 11:56:11 +0800

    zero: wifi: set TARGET_USES_64_BIT_BCMDHD

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,6 +162,7 @@ ifneq ($(strip $(wildcard $(TOP)/bootable/recovery/variables.h)),)
 endif
 
 # Wifi
+TARGET_USES_64_BIT_BCMDHD        := true
 BOARD_HAVE_SAMSUNG_WIFI          := true
 BOARD_WLAN_DEVICE                := bcmdhd
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
