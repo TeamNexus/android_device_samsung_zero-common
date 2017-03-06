a161c5fc339a52b84a17233f1248ff0099bc3bb2
Usaamah Patel <ussyp2015@gmail.com>
2017-03-06 17:45:41 +0000

    Add Support for T/W8
    * This should fix Incall Bug for T/W8 Variants

--- include/samsung_audio.h
+++ include/samsung_audio.h
@@ -52,5 +52,13 @@
 #define SOUND_CAPTURE_HOTWORD_DEVICE 0
 */
 
+/*
+ * Some device variants (often T-Mobile) have a separate voice processing IC
+ * (Audience EarSmart xxx).
+ * This hooks into the voice call session and enables, configures and disables
+ * this extra firmware so RX/TX streams can be routed by the driver.
+ */
+#define AUDIENCE_EARSMART_IC
+
 
 #endif // SAMSUNG_AUDIO_H
