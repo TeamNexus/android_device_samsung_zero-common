49dad18d339ad427b1c5393646f1af87f8d82734
Lukas0610 <mail@lukasberger.at>
2017-03-23 09:41:35 +0100

    zero: splitted device-configs

---
+++ configs/device/common.mk
@@ -0,0 +1,82 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 Team Nexus7420
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Common Overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/zero-common/overlay
+
+PRODUCT_AAPT_CONFIG := xlarge
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+# A list of dpis to select prebuilt apk, in precedence order.
+PRODUCT_AAPT_PREBUILT_DPI := hdpi mdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
+
+# Browser
+PRODUCT_PACKAGES += \
+Gello
+
+# Dbthtool
+PRODUCT_PACKAGES += \
+    dtbhtoolExynos
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.universal7420
+
+# Libstlport
+PRODUCT_PACKAGES += \
+    libstlport
+
+# MDNIE
+PRODUCT_PACKAGES += \
+    AdvancedDisplay
+
+# MobiCore
+PRODUCT_PACKAGES += \
+    mcDriverDaemon
+
+# Power
+PRODUCT_PACKAGES += \
+    power.universal7420
+
+# Radio
+PRODUCT_PACKAGES += \
+    libxml2 \
+    libprotobuf-cpp-full
+
+# Shims
+PRODUCT_PACKAGES += \
+    libshim_bauthtzcommon
+
+# Torch
+PRODUCT_PACKAGES += \
+    Torch
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Snap
+# PRODUCT_PACKAGES += \
+#    Snap
---
+++ configs/device/properties.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 Team Nexus7420
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# common build properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=560 \
+    ro.opengles.version=196609 \
+    ro.chipname=exynos7420 \
+    af.fast_track_multiplier=1 \
+    audio_hal.force_voice_config=wide \
+    ro.nfc.sec_hal=true \
+    wifi.interface=wlan0 \
+    debug.hwc.force_gpu=1 \
+    ro.bq.gpu_to_cpu_unsupported=1 \
+    ro.bt.bdaddr_path="/efs/bluetooth/bt_addr" \
+    qcom.bluetooth.soc=rome
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib64/libsec-ril.so \
+    rild.libargs=-d /dev/ttyS0 \
+    ro.telephony.ril_class=zeroRIL \
+    telephony.lteOnGsmDevice=1 \
+    telephony.lteOnCdmaDevice=0 \
+    ro.telephony.default_network=9 \
+    ro.use_data_netmgrd=false \
+    persist.data.netmgrd.qos.enable=false \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10
