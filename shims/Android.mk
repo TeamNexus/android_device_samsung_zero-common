be3bcdb5fd4af4988029630ad415194547033608
Usaamah Patel <ussyp2015@gmail.com>
2017-03-26 14:28:12 +0100

    Add FP shim and Update Blob List
    Signed-off-by: Usaamah Patel <ussyp2015@gmail.com>

--- shims/Android.mk
+++ shims/Android.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 Team Nexus7420
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,8 +14,6 @@
 # limitations under the License.
 #
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    fingerprintd \
-    fingerprint.exynos5
-    libbauthtzcommon_shim
+LOCAL_PATH := $(call my-dir)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
--- shims/libbauthtzcommon/Android.mk
+++ shims/libbauthtzcommon/Android.mk
@@ -1,6 +1,4 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 Team Nexus7420
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,10 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    fingerprintd \
-    fingerprint.exynos5
-    libbauthtzcommon_shim
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    libbauthtzcommon.c
+
+LOCAL_SHARED_LIBRARIES := liblog
+
+LOCAL_MODULE := libbauthtzcommon_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
