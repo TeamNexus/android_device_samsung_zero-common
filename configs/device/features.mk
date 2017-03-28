b8037c70f92a17bf7cf5b08c51acb7eda369cfd3
Lukas0610 <mail@lukasberger.at>
2017-03-28 12:16:59 +0200

    init: fixed initial permissions

--- configs/device/features.mk
+++ configs/device/features.mk
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 
-# Permissions
+# Features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
